5a49a304f6a3a05f66b847d97d7903e5aa5c79a9
RyanMelo <ryangarciamelo@gmail.com>
2020-08-28 11:56:47 -0300

    Segundo commit

---
+++ src/exercico_pratica/lista_02/quest_01.java
@@ -0,0 +1,24 @@
+package exercico_pratica.lista_02;
+import java.util.Scanner;
+
+public class quest_01 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+    
+        System.out.print("Digite o primeiro numero: ");
+        int numeroUm = entrada.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        int numeroDois = entrada.nextInt();
+
+        int soma = numeroUm + numeroDois;
+        int subtracao = numeroUm - numeroDois;
+        int multi = numeroUm * numeroDois;
+        float divisao = numeroUm / numeroDois;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtracao: " + subtracao);
+        System.out.println("Multiplicacao: " + multi);
+        System.out.println("Divisao: " + divisao);
+    }
+}
---
+++ src/exercico_pratica/lista_02/quest_02.java
@@ -0,0 +1,18 @@
+package exercico_pratica.lista_02;
+import java.util.Scanner;
+
+public class quest_02 {
+    public static void main(String[] args) {
+        Scanner entrada  = new Scanner(System.in);
+
+        System.out.print("Distancia KM: ");
+        float distancia = entrada.nextFloat();
+
+        System.out.print("Total de combustível gastos R$: ");
+        float totalGasto = entrada.nextFloat();
+
+        float consumoMedio = totalGasto / distancia;
+
+        System.out.println("Consumo medio R$ " + consumoMedio);
+    }
+}

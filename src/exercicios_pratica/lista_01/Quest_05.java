beae1f9a5c6ef736e19801a81820c338919681c9
RyanMelo <ryangarciamelo@gmail.com>
2020-08-29 00:43:20 -0300

    Conclusao da lista 01

---
+++ src/exercicios_pratica/lista_01/Quest_05.java
@@ -0,0 +1,34 @@
+package exercicios_pratica.lista_01;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Quest_05 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int valorJ;
+        int qtdQuadrados = 0;
+
+        do{
+            System.out.print("Digite o valor de J: ");
+            valorJ = entrada.nextInt();
+
+            if(valorJ != 0){
+                double raiz = Math.sqrt(valorJ);
+
+                if(Math.pow(raiz, 2) == valorJ) {
+                    qtdQuadrados += 1;
+                    // System.out.println("Sim");
+
+                }
+                
+            } else {
+                System.out.println("Programa finalizado!");
+                break;
+            }
+
+        }while(valorJ != 0);
+
+        System.out.println("Quantidade de quadrados: " + qtdQuadrados);
+
+    }
+}
---
+++ src/exercicios_pratica/lista_01/Quest_06.java
@@ -0,0 +1,51 @@
+package exercicios_pratica.lista_01;
+import java.util.Scanner;
+
+public class Quest_06 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int m;
+
+        do{
+            System.out.print("Digite um numero: ");
+            m = entrada.nextInt();
+
+            if(m < 10) {
+                int fatorialM = calculaFatorial(m);
+                System.out.println("Fatorial de " + m + " é: " + fatorialM);
+            } else {
+                int qtdDivisor = numerDivisores(m);
+                System.out.println("Numero de divisores de " + m + " é: " + qtdDivisor);
+            }
+
+        }while(m != 999);
+
+    }
+
+    public static int calculaFatorial(int numero) {
+        int fat = 1;
+        for(int i = 1; numero > i; numero--){
+            fat *= numero;
+        }
+
+        return fat;
+    }
+
+    public static int numerDivisores(int numero) {
+        int divisao;
+        int qtdDivisores = 0;
+       
+        for(int i = 1; i <= numero; i++) {
+            divisao = numero / i;
+            
+            if(divisao * i == numero){
+                qtdDivisores++;
+            }
+
+        }
+
+        return qtdDivisores;
+    }
+
+
+}
